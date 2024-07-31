638cac5f00a1712f55883e0d7aa9c5ae0b41ff5b
EduardoDametto25 <damettoed@gmail.com>
2024-07-31 12:21:34 -0300

    Começo do jogo da velha, botamos as formas e os espaçaos

---
+++ jogo-da-velha/componets/figura.js
@@ -0,0 +1,18 @@
+import { TouchableOpacity } from 'react-native'
+import React from 'react'
+import {Entypo} from '@expo/vector-icons'
+
+export default function Figura({ vetor, posicao, clicado }) {
+    const getIcon = () => {
+        if (vetor[posicao] !== 0) {
+            return vetor[posicao] ? "cross" : "circle";
+        }
+        return "pencil";
+    }
+
+    return (
+    <TouchableOpacity onPress={clicado}>
+        <Entypo name={getIcon()} size={90} color="#000" />
+    </TouchableOpacity>
+  )
+}
